8897260b84f4bd678e8f0c28f9f708e2ac181851
BRIZEIUM <64617361+BRIZEIUM@users.noreply.github.com>
2026-05-31 10:58:01 +0800

    Released via Polished Semantic Colorizer

--- SemanticColorizer/Properties/AssemblyInfo.cs
+++ SemanticColorizer/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SonarColorizer")]
+[assembly: AssemblyTitle("PolishedSemanticColorizer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("LinkingMountains")]
-[assembly: AssemblyProduct("SonarColorizer")]
+[assembly: AssemblyProduct("PolishedSemanticColorizer")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.4.1.*")]
+[assembly: AssemblyVersion("1.4.3.*")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
