1edd20d0caad1776402bad5213d53857435bf052
BRIZEIUM <64617361+BRIZEIUM@users.noreply.github.com>
2026-07-25 06:54:27 +0800

    Released via Noetphosiris

--- SemanticColorizer/Properties/AssemblyInfo.cs
+++ SemanticColorizer/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CodeSpectrum")]
+[assembly: AssemblyTitle("Noetphosiris")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("LinkingMountains")]
-[assembly: AssemblyProduct("CodeSpectrum")]
+[assembly: AssemblyProduct("Noetphosiris")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.4.5.*")]
+[assembly: AssemblyVersion("1.4.6.*")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
